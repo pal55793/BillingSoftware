8ce00b7522f4815a0e8b1810a35cf4baa369d546
Rohit Pal <rohpal3@publicisgroupe.net>
2026-03-10 16:32:31 +0530

    Change to war

--- BillingSfotware/src/main/java/com/rk/BillingSfotwareApplication.java
+++ BillingSfotware/src/main/java/com/rk/BillingSfotwareApplication.java
@@ -2,12 +2,20 @@ package com.rk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @SpringBootApplication
 @CrossOrigin
-public class BillingSfotwareApplication {
+public class BillingSfotwareApplication extends SpringBootServletInitializer {
 
+	 // You MUST add this method to support WAR deployment
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BillingSfotwareApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(BillingSfotwareApplication.class, args);
 	}
